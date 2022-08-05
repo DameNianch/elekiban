ce9246600a489b3e1fb7029def40698da7aa1462
kenkoba <medaca.pro@gmail.com>
2022-08-05 20:48:21 +0900

    a

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ install_requires = [
 setup(
     name='elekiban',
     version='0.0.0',
+    url="https://github.com/DameNianch/elekiban",
+    author="DameNianch",
+    license="Check https://github.com/DameNianch/elekiban",
     packages=find_packages(),
     install_requires=install_requires
 )
