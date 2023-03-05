f99cb94b8353c790eed2d2a1dd1bca53ffecb174
ponkotsu <40750603+DameNianch@users.noreply.github.com>
2023-03-05 23:51:12 +0900

    Revert "Release/0.0.5"

--- elekiban/pipeline/pipe.py
+++ elekiban/pipeline/pipe.py
@@ -2,7 +2,6 @@ from abc import ABCMeta
 from abc import abstractmethod
 from argparse import ArgumentError
 from dataclasses import dataclass
-import os
 from typing import List
 import cv2
 import numpy as np
@@ -15,7 +14,7 @@ def through(x):
 
 
 class AbstractPipe(metaclass=ABCMeta):
-    def __init__(self, pipe_name: str, data_num: int, adjust_fn, batch_fn) -> None:
+    def __init__(self, pipe_name: str, data_num: int, adjust_fn=through, batch_fn=through) -> None:
         self.pipe_name = pipe_name
         self.data_num = data_num
         self._adjust_fn = adjust_fn
@@ -37,55 +36,56 @@ class IOPipes:
 
 
 class ImagePipe(AbstractPipe):
-    def __init__(self, pipe_name, image_paths, adjust_fn=through, batch_fn=np.array) -> None:
+    def __init__(self, pipe_name, image_paths, adjust_fn=through, batch_fn=through) -> None:
         super().__init__(pipe_name=pipe_name, data_num=len(image_paths), adjust_fn=adjust_fn, batch_fn=batch_fn)
         self._image_paths = image_paths
         self._setup()
 
     def generate(self, indices):
-        return self._batch_fn([self._adjust_fn(cv2.imread(self._image_paths[i])) for i in indices])
+        return self._batch_fn(np.array([self._adjust_fn(cv2.imread(self._image_paths[i])) for i in indices]))
 
     def _setup(self):
         for i_path in self._image_paths:
-            # HACK: cv2 does not raise Error...
-            # cv2.imread(i_path)
-            if not os.path.isfile(i_path):
-                raise FileExistsError
+            try:
+                cv2.imread(i_path)
+            except BaseException:
+                print(f"Cannot open {i_path}")
+        print("ok")
 
 
 class LabelPipe(AbstractPipe):
-    def __init__(self, pipe_name, labels, adjust_fn=through, batch_fn=np.array) -> None:
+    def __init__(self, pipe_name, labels, adjust_fn=through, batch_fn=through) -> None:
         super().__init__(pipe_name=pipe_name, data_num=len(labels), adjust_fn=adjust_fn, batch_fn=batch_fn)
         self._labels = labels
 
     def generate(self, indices):
-        return self._batch_fn([self._adjust_fn(self._labels[i]) for i in indices])
+        return self._batch_fn(np.array([self._adjust_fn(self._labels[i]) for i in indices]))
 
     def _setup(self):
         pass
 
 
 class CustomFunctionPipe(AbstractPipe):
-    def __init__(self, pipe_name, custom_fn, adjust_fn=through, batch_fn=np.array, data_num=100) -> None:
+    def __init__(self, pipe_name, custom_fn, adjust_fn=through, batch_fn=through, data_num=100) -> None:
         super().__init__(pipe_name=pipe_name, data_num=data_num, adjust_fn=adjust_fn, batch_fn=batch_fn)
         # TODO: Rethink the relationship between Pump and CustomFunctionPipe.
         self._custom_fn = custom_fn
         self._setup()
 
     def generate(self, indices):
-        return self._batch_fn([self._adjust_fn(self._custom_fn(i)) for i in indices])
+        return self._batch_fn(np.array([self._adjust_fn(self._custom_fn(i)) for i in indices]))
 
     def _setup(self):
         pass
 
 
 class PipeWithPump(AbstractPipe):
-    def __init__(self, pipe_name: str, data_pump: AbstractDataPump, adjust_fn=through, batch_fn=np.array) -> None:
+    def __init__(self, pipe_name: str, data_pump: AbstractDataPump, adjust_fn=through, batch_fn=through) -> None:
         super().__init__(pipe_name=pipe_name, data_num=len(data_pump), adjust_fn=adjust_fn, batch_fn=batch_fn)
         self._data_pump = data_pump
 
     def generate(self, indices):
-        return self._batch_fn([self._adjust_fn(self._data_pump[i]) for i in indices])
+        return self._batch_fn(np.array([self._adjust_fn(self._data_pump[i]) for i in indices]))
 
     def _setup(self):
         pass
